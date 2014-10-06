393d8c6f21508f3e2482ba2151c667afd3859b2c
Eric Her <ericbher@gmail.com>
2014-10-06 17:25:30 -0500

    finished1

--- week2/99bottles/99bottles.js
+++ week2/99bottles/99bottles.js
@@ -1,3 +1,15 @@
+var numBeers = prompt("How many bottles of beer are there on the wall?");
+
+function songStart(bottles){
+	for (var i = numBeers; i > 2; i--){
+		document.write (i + ' bottles of beer on the wall, '  + i + ' bottles of beer. <br />' + ' Take one down and pass it around, ' + (i - 1) + ' bottles of beer on the wall. <br />')
+	}
+	document.write ('2 bottles of beer on the wall, 2 bottles of beer. <br /> Take one down and pass it around, 1 bottle of beer on the wall. <br />')
+	document.write ('1 bottle of beer on the wall, 1 bottle of beer. <br />' + ' Take one down and pass it around, ' + ' no more bottles of beer on the wall. <br />')
+	document.write ('No more bottles of beer on the wall, no more bottles of beer. <br /> Go to the store and buy some more, 99 bottles of beer on the wall.')
+}
+
+songStart(numBeers);
 /* 
 1. Using the javascript keyword prompt ask the user for the number of bottles of beer on the wall and output the lyrics of the song to the page.
 
