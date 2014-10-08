caaf83d3a0c3677d20409746159c69bb7ff0a4ef
Eric Her <ericbher@gmail.com>
2014-10-08 17:04:44 -0500

    highlow game

--- week2/highlowgame/game.js
+++ week2/highlowgame/game.js
@@ -6,3 +6,68 @@ Stretch Challenge 1: Keep track of the number of guesses the user took to get to
 
 Stretch Challenge 2: Allow the operator of the game to limit the number of user guesses by setting a constant variable named "NUM_MAX_GUESSES"
 */
+
+/*function guessGame(){
+
+var guessCount = 0;
+
+var NUM_MAX_GUESSES = 10;
+
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+var numberGuess = document.getElementById('guess').value;
+
+
+for (var i = 0; i < NUM_MAX_GUESSES; i++){
+	if (numberGuess == randomNumber) {
+	document.write('You guessed the number!');
+	break;
+} 
+
+else if (numberGuess > randomNumber) {
+	document.write('You are too high');
+	guessCount++;
+	
+}
+
+else if (numberGuess < randomNumber){
+	document.write('You are too low');
+	guessCount++;
+}
+
+}
+document.write('It took you ' + guessCount + ' out of ' + NUM_MAX_GUESSES + ' guesses.');
+
+} */
+
+
+
+var guessCount = 0;
+
+var NUM_MAX_GUESSES = 10;
+
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+for (var i = 0; i < NUM_MAX_GUESSES; i++){
+	var numberGuess = prompt("Guess a number between 1 and 100");
+	if (numberGuess == randomNumber) {
+	console.log ('You guessed the number!');
+	break;
+} 
+
+else if (numberGuess > randomNumber) {
+	console.log ('You are too high');
+	guessCount++;
+}
+
+else if (numberGuess < randomNumber){
+	console.log('You are too low');
+	guessCount++;
+}
+
+}
+
+console.log('It took you ' + guessCount + ' out of ' + NUM_MAX_GUESSES + ' guesses.'); 
+
+
+
